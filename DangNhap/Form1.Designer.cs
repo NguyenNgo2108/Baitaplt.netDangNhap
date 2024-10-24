9cd58034e7450834bf19f2df64f185ca0f9d5076
NguyenNgo2108 <nguyenngo2102@gmail.com>
2024-10-24 14:47:49 +0700

    dang nhap

---
+++ DangNhap/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DangNhap
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbTenDangNhap = new TextBox();
+            tbTaiKhoan = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnDangNhap = new Button();
+            btnThoat = new Button();
+            SuspendLayout();
+            // 
+            // tbTenDangNhap
+            // 
+            tbTenDangNhap.Location = new Point(316, 139);
+            tbTenDangNhap.Name = "tbTenDangNhap";
+            tbTenDangNhap.Size = new Size(125, 27);
+            tbTenDangNhap.TabIndex = 0;
+            tbTenDangNhap.TextChanged += tbTenDangNhap_TextChanged;
+            // 
+            // tbTaiKhoan
+            // 
+            tbTaiKhoan.Location = new Point(316, 197);
+            tbTaiKhoan.Name = "tbTaiKhoan";
+            tbTaiKhoan.Size = new Size(125, 27);
+            tbTaiKhoan.TabIndex = 1;
+            tbTaiKhoan.TextChanged += tbTaiKhoan_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(177, 142);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Tên Đăng Nhập:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(217, 204);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Mật Khẩu:";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(329, 51);
+            label3.Name = "label3";
+            label3.Size = new Size(82, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Đăng nhập";
+            // 
+            // btnDangNhap
+            // 
+            btnDangNhap.Location = new Point(329, 269);
+            btnDangNhap.Name = "btnDangNhap";
+            btnDangNhap.Size = new Size(94, 29);
+            btnDangNhap.TabIndex = 5;
+            btnDangNhap.Text = "Đăng Nhập";
+            btnDangNhap.UseVisualStyleBackColor = true;
+            btnDangNhap.Click += btnDangNhap_Click;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(329, 326);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(94, 29);
+            btnThoat.TabIndex = 6;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            btnThoat.Click += btnThoat_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnThoat);
+            Controls.Add(btnDangNhap);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(tbTaiKhoan);
+            Controls.Add(tbTenDangNhap);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbTenDangNhap;
+        private TextBox tbTaiKhoan;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnDangNhap;
+        private Button btnThoat;
+    }
+}
---
+++ DangNhap/Form1.cs
@@ -0,0 +1,45 @@
+﻿namespace DangNhap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbTenDangNhap_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbTaiKhoan_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string tenDangNhap = tbTenDangNhap.Text;
+            string matKhau = tbTaiKhoan.Text;
+
+            if (tenDangNhap == "admin" && matKhau == "1234")
+            {
+                MessageBox.Show("Đăng nhập thành công");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại");
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
